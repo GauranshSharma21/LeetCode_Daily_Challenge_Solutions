f1fb4145bade58ae89bd87c0553be885ca23eec3
GauranshSharma21 <2k24.cs1i.2414407@gmail.com>
2026-08-06 06:37:45 +0530

    Time: 0 ms (100%), Space: 9.4 MB (3.33%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,19 +1,18 @@
+//string approach ->
+//complexities[https://chatgpt.com/share/6a73dcab-e390-83ee-a0d9-67ace21fc5bc]
 
+//REVISE ITS COMPLEXITY ANALYSIS
 class Solution {
 public:
     int smallestNumber(int n, int t) {
         while(true){
-            int product= 1;
-            int temp = n;
+            int product = 1;
 
-            while(temp > 0){
-                product *= (temp % 10);
-                temp /= 10;
-            }
+            string s = to_string(n);
+            for(char x : s) product *= x - '0';
 
             if(product % t == 0) return n;
-
-            n++;
+            n++; 
         }
     }
 };
